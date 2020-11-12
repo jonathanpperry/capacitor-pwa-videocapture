25f6f41d27ae7374257247f69153f97b248494e9
Jonathan Perry <jpper@umich.edu>
2020-11-12 14:09:33 +0900

    Finished tutorial, though seemingly with an error

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,88 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  ViewChild,
+} from "@angular/core";
+import { VideoService } from "../services/video.service";
+
+import { Capacitor, Plugins } from "@capacitor/core";
+import * as WebVPPlugin from "capacitor-video-player";
+const { CapacitorVideoPlayer } = Plugins;
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"],
 })
-export class HomePage {
+export class HomePage implements AfterViewInit {
+  mediaRecorder: MediaRecorder;
+  videoPlayer: any;
+  isRecording = false;
+  videos = [];
+  @ViewChild("video") captureElement: ElementRef;
+  constructor(
+    private videoService: VideoService,
+    private changeDetector: ChangeDetectorRef
+  ) {}
 
-  constructor() {}
+  async ngAfterViewInit() {
+    this.videos = await this.videoService.loadVideos();
+    // Initialize the video player plugin
+    if (Capacitor.isNative) {
+      this.videoPlayer = CapacitorVideoPlayer;
+    } else {
+      this.videoPlayer = WebVPPlugin.CapacitorVideoPlayer;
+    }
+  }
 
+  async recordVideo() {
+    const stream = await navigator.mediaDevices.getUserMedia({
+      video: {
+        facingMode: "user",
+      },
+      audio: true,
+    });
+    this.captureElement.nativeElement.srcObject = stream;
+    // Set recording boolean
+    this.isRecording = true;
+    const options = { mimeType: "video/webm" };
+    this.mediaRecorder = new MediaRecorder(stream, options);
+    let chunks = [];
+
+    this.mediaRecorder.ondataavailable = (event) => {
+      if (event.data && event.data.size > 0) {
+        chunks.push(event.data);
+      }
+    };
+    this.mediaRecorder.onstop = async (event) => {
+      const videoBuffer = new Blob(chunks, { type: "video/webm" });
+      // Store the video
+      await this.videoService.storeVideo(videoBuffer);
+
+      // reload the list
+      this.videos = this.videoService.videos;
+      this.changeDetector.detectChanges();
+    };
+  }
+
+  stopRecord() {
+    this.mediaRecorder.stop();
+    this.mediaRecorder = null;
+    this.captureElement.nativeElement.srcObject = null;
+    this.isRecording = false;
+  }
+
+  async play(video) {
+    const base64data = await this.videoService.getVideoUrl(video);
+
+    // Show the player fullscreen
+    await this.videoPlayer.initPlayer({
+      mode: "fullscreen",
+      url: base64data,
+      playerId: "fullscreen",
+      componentTag: "app-home",
+    });
+  }
 }
--- src/app/services/video.service.ts
+++ src/app/services/video.service.ts
@@ -1,9 +1,55 @@
-import { Injectable } from '@angular/core';
-
+import { Injectable } from "@angular/core";
+import { Plugins, FilesystemDirectory } from "@capacitor/core";
+const { Filesystem, Storage } = Plugins;
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class VideoService {
+  public videos = [];
+  private VIDEOS_KEY: string = "videos";
+  constructor() {}
 
-  constructor() { }
+  async loadVideos() {
+    const videoList = await Storage.get({ key: this.VIDEOS_KEY });
+    this.videos = JSON.parse(videoList.value) || [];
+    return this.videos;
+  }
+
+  async storeVideo(blob) {
+    const fileName = new Date().getTime() + ".mp4";
+    const base64Data = (await this.convertBlobToBase64(blob)) as string;
+
+    const savedFile = await Filesystem.writeFile({
+      path: fileName,
+      data: blob,
+      directory: FilesystemDirectory.Data,
+    });
+
+    // Add the element to the front of videos array
+    this.videos.unshift(savedFile.uri);
+
+    return Storage.set({
+      key: this.VIDEOS_KEY,
+      value: JSON.stringify(this.videos),
+    });
+  }
+
+  private convertBlobToBase64 = (blob: Blob) =>
+    new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onerror = reject;
+      reader.onload = () => {
+        resolve(reader.result);
+      };
+      reader.readAsDataURL(blob);
+    });
+
+  async getVideoUrl(fullPath) {
+    const path = fullPath.substr(fullPath.lastIndexOf("/") + 1);
+    const file = await Filesystem.readFile({
+      path: path,
+      directory: FilesystemDirectory.Data,
+    });
+    return `data:video/mp4;base64,${file.data}`;
+  }
 }
